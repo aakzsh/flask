d259b7e886d928b8e046f0eeefab167a966c67ce
Aakash Shrivastava <69726390+aakzsh@users.noreply.github.com>
2023-11-23 20:43:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify
 import os
 import pandas as pd
 import pickle
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -11,7 +13,12 @@ def index():
 
 @app.route('/check')
 def checkk():
-    return "69"    
+    return "99"    
+
+@app.route("/test")
+def test():
+    return jsonify({"sender_score": 0.421, "receiver_score": 0.1112, "overall_score": 0.232})
+
 
 @app.route('/calculate/<fromm>/<to>/<amount>')
 def check(fromm, to, amount):
